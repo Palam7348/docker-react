bd920324ac8460ea6daadea1dbf7e8a0b2f9dec3
Sergey Palamarchuk <serega217348@gmail.com>
2023-11-27 18:35:58 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there, Harry Potter
+          Hello there, Harry Potter and Germiona
         </p>
         <a
           className="App-link"
